f24d38747ea15b0fd79f131e853cdebcd1d6fd8a
Ricardo Delgado <Ricardo.Delgado@endava.com>
2022-02-16 20:30:53 -0500

    Create files and project structure

---
+++ src/main/java/com/rockcor/strategydesingpattern/service/Context.java
@@ -0,0 +1,13 @@
+package com.rockcor.strategydesingpattern.service;
+
+public class Context {
+    Strategy strategy;
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void doSomething(){
+
+    }
+}
---
+++ src/main/java/com/rockcor/strategydesingpattern/service/strategies/WalkStrategy.java
@@ -0,0 +1,16 @@
+package com.rockcor.strategydesingpattern.service.strategies;
+
+import com.rockcor.strategydesingpattern.model.Route;
+import com.rockcor.strategydesingpattern.service.Strategy;
+
+public class WalkStrategy implements Strategy {
+
+    @Override
+    public Route buildRoute() {
+        Route r = new Route();
+        r.setDuration("\u231B \u231B \u231B \u231B \u231B");
+        r.setPrice("\u0024");
+
+        return r;
+    }
+}
